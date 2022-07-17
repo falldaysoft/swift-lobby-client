98489782426b5826c3518ccfbc0c585ec0b7aaae
Steve Tibbett <steve@fallday.ca>
2022-07-17 08:04:10 -0400

    Update library target name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftLobbyClient",
+            name: "swift-lobby-client",
             targets: ["swift-lobby-client"]),
     ],
     dependencies: [
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swift-lobby-client",
+            name: "SwiftLobbyClient",
             dependencies: []),
         .testTarget(
             name: "swift-lobby-clientTests",
